645488fc14ddf973f2e78a6efa5d763713abae12
D'ANNA PIERRE <p.danna@santevet.com>
2024-02-19 12:23:36 +0100

    feat/Translated to English and go back to main Menu when the action is done

--- main.go
+++ main.go
@@ -8,17 +8,15 @@ import (
     "github.com/rivo/tview"
 )
 
-// Structure pour stocker les informations des instances
 type InstanceInfo struct {
     Id    string
     Name  string
     State string
 }
 
-// Créer une session AWS
 func createSession() (*ec2.EC2, error) {
     sess, err := session.NewSession(&aws.Config{
-        Region: aws.String("eu-west-1"), // Changez cela selon votre région
+        Region: aws.String("eu-west-1"), // Change this according to your region
     })
     if err != nil {
         return nil, err
@@ -26,7 +24,6 @@ func createSession() (*ec2.EC2, error) {
     return ec2.New(sess), nil
 }
 
-// Récupérer les informations des instances EC2
 func getInstancesInfo(svc *ec2.EC2) ([]InstanceInfo, error) {
     input := &ec2.DescribeInstancesInput{}
     result, err := svc.DescribeInstances(input)
@@ -37,7 +34,7 @@ func getInstancesInfo(svc *ec2.EC2) ([]InstanceInfo, error) {
     var instances []InstanceInfo
     for _, reservation := range result.Reservations {
         for _, instance := range reservation.Instances {
-            name := "No Name" // Nom par défaut si aucune balise Name n'est trouvée
+            name := "No Name" // Default name if no Name tag is found
             for _, tag := range instance.Tags {
                 if *tag.Key == "Name" {
                     name = *tag.Value
@@ -51,11 +48,9 @@ func getInstancesInfo(svc *ec2.EC2) ([]InstanceInfo, error) {
             })
         }
     }
-
     return instances, nil
 }
 
-// Fonction pour démarrer une instance
 func startInstance(svc *ec2.EC2, instanceId string) error {
     input := &ec2.StartInstancesInput{
         InstanceIds: []*string{aws.String(instanceId)},
@@ -64,7 +59,6 @@ func startInstance(svc *ec2.EC2, instanceId string) error {
     return err
 }
 
-// Fonction pour arrêter une instance
 func stopInstance(svc *ec2.EC2, instanceId string) error {
     input := &ec2.StopInstancesInput{
         InstanceIds: []*string{aws.String(instanceId)},
@@ -73,66 +67,73 @@ func stopInstance(svc *ec2.EC2, instanceId string) error {
     return err
 }
 
-func main() {
-    svc, err := createSession()
+func showInstancesList(app *tview.Application, svc *ec2.EC2) {
+    instancesInfo, err := getInstancesInfo(svc)
     if err != nil {
-        fmt.Println("Erreur lors de la création de la session AWS:", err)
+        fmt.Println("Error retrieving EC2 instance information:", err)
         return
     }
 
-    instancesInfo, err := getInstancesInfo(svc)
+    list := tview.NewList()
+    for _, info := range instancesInfo {
+        displayText := fmt.Sprintf("%s (%s) - %s", info.Name, info.Id, info.State)
+        list.AddItem(displayText, "", 0, makeInstanceSelectionHandler(app, svc, info))
+    }
+
+    list.SetBorder(true).SetTitle("Select an instance").SetBorderPadding(1, 1, 1, 1)
+    app.SetRoot(list, true).SetFocus(list)
+}
+
+func makeInstanceSelectionHandler(app *tview.Application, svc *ec2.EC2, info InstanceInfo) func() {
+    return func() {
+        actionModal := tview.NewModal().
+            SetText(fmt.Sprintf("Do you want to start or stop %s?", info.Id)).
+            AddButtons([]string{"Start", "Stop", "Cancel"}).
+            SetDoneFunc(func(buttonIndex int, buttonLabel string) {
+                switch buttonLabel {
+                case "Start", "Stop":
+                    confirmText := fmt.Sprintf("Are you sure you want to %s %s?", buttonLabel, info.Id)
+                    confirmationModal := tview.NewModal().
+                        SetText(confirmText).
+                        AddButtons([]string{"Confirm", "Cancel"}).
+                        SetDoneFunc(func(confirmIndex int, confirmLabel string) {
+                            if confirmLabel == "Confirm" {
+                                if buttonLabel == "Start" {
+                                    err := startInstance(svc, info.Id)
+                                    if err != nil {
+                                        fmt.Println("Error starting the instance:", err)
+                                    }
+                                } else {
+                                    err := stopInstance(svc, info.Id)
+                                    if err != nil {
+                                        fmt.Println("Error stopping the instance:", err)
+                                    }
+                                }
+                                showInstancesList(app, svc) // Reload the instance list
+                            } else {
+                                showInstancesList(app, svc) // Return to the list if "Cancel" is selected
+                            }
+                        })
+                    app.SetRoot(confirmationModal, false).SetFocus(confirmationModal)
+                default:
+                    showInstancesList(app, svc) // Return to the list if "Cancel" is selected in the first modal
+                }
+            })
+        app.SetRoot(actionModal, false).SetFocus(actionModal)
+    }
+}
+
+func main() {
+    svc, err := createSession()
     if err != nil {
-        fmt.Println("Erreur lors de la récupération des informations des instances EC2:", err)
+        fmt.Println("Error creating AWS session:", err)
         return
     }
 
     app := tview.NewApplication()
-    list := tview.NewList()
-    for _, info := range instancesInfo {
-        info := info // Capture la variable dans la portée de la closure
-        displayText := fmt.Sprintf("%s (%s) - %s", info.Name, info.Id, info.State)
-        list.AddItem(displayText, "", 0, func(info InstanceInfo) func() {
-            return func() {
-                actionModal := tview.NewModal().
-                    SetText(fmt.Sprintf("Voulez-vous démarrer ou arrêter %s?", info.Id)).
-                    AddButtons([]string{"Démarrer", "Arrêter", "Annuler"}).
-                    SetDoneFunc(func(buttonIndex int, buttonLabel string) {
-                        switch buttonLabel {
-                        case "Démarrer", "Arrêter":
-                            confirmText := fmt.Sprintf("Êtes-vous sûr de vouloir %s %s?", buttonLabel, info.Id)
-                            confirmationModal := tview.NewModal().
-                                SetText(confirmText).
-                                AddButtons([]string{"Confirmer", "Annuler"}).
-                                SetDoneFunc(func(confirmIndex int, confirmLabel string) {
-                                    if confirmLabel == "Confirmer" {
-                                        if buttonLabel == "Démarrer" {
-                                            if err := startInstance(svc, info.Id); err != nil {
-                                                fmt.Printf("Erreur lors du démarrage de %s: %v\n", info.Id, err)
-                                            } else {
-                                                fmt.Printf("Instance démarrée: %s\n", info.Id)
-                                            }
-                                        } else if buttonLabel == "Arrêter" {
-                                            if err := stopInstance(svc, info.Id); err != nil {
-                                                fmt.Printf("Erreur lors de l'arrêt de %s: %v\n", info.Id, err)
-                                            } else {
-                                                fmt.Printf("Instance arrêtée: %s\n", info.Id)
-                                            }
-                                        }
-                                    }
-                                    app.Stop()
-                                })
-                            app.SetRoot(confirmationModal, false).SetFocus(confirmationModal)
-                        default:
-                            app.Stop()
-                        }
-                    })
-                app.SetRoot(actionModal, false).SetFocus(actionModal)
-            }
-        }(info))
-    }
+    showInstancesList(app, svc)
 
-    list.SetBorder(true).SetTitle("Sélectionnez une instance").SetBorderPadding(1, 1, 1, 1)
-    if err := app.SetRoot(list, true).SetFocus(list).Run(); err != nil {
-        fmt.Printf("Erreur lors de l'exécution de l'application: %v\n", err)
+    if err := app.Run(); err != nil {
+        fmt.Printf("Error running the application: %v\n", err)
     }
 }
